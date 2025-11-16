fbcea52ecc07b0455f394d62a625d60db1b96b7c
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-11-16 11:58:17 +0530

    Update IssuesViewModel.kt

--- app/src/main/java/com/vanshika/parkit/user/viewmodel/IssuesViewModel.kt
+++ app/src/main/java/com/vanshika/parkit/user/viewmodel/IssuesViewModel.kt
@@ -1,18 +1,19 @@
 package com.vanshika.parkit.user.viewmodel
 
-import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import com.vanshika.parkit.user.data.model.IssuesDataClass
 import com.vanshika.parkit.user.data.repository.IssuesRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class IssuesViewModel @Inject constructor(
     private val issuesRepository: IssuesRepository
-): ViewModel(){
-    var allIssues = mutableStateListOf<IssuesDataClass>()
-        private set
+) : ViewModel() {
+    private val _allIssues = MutableStateFlow<List<IssuesDataClass>>(emptyList())
+    val allIssues: StateFlow<List<IssuesDataClass>> = _allIssues
 
     init {
         fetchAllIssues()
@@ -20,18 +21,18 @@ class IssuesViewModel @Inject constructor(
 
     fun addIssue(issue: IssuesDataClass) = issuesRepository.addIssue(issue)
 
-    fun fetchAllIssues(){
+    fun fetchAllIssues() {
         issuesRepository.fetchAllIssues { issues ->
-            allIssues.clear()
-            allIssues.addAll(issues)
+            _allIssues.value = issues
         }
     }
 
     fun getIssueById(issueId: String): IssuesDataClass? {
-        return allIssues.find { it.issueId == issueId }
+        return _allIssues.value.find { it.issueId == issueId }
     }
 
-    fun updateIssueStatus(issueId: String, newStatus: String) = issuesRepository.updateIssueStatus(issueId, newStatus)
+    fun updateIssueStatus(issueId: String, newStatus: String) =
+        issuesRepository.updateIssueStatus(issueId, newStatus)
 
     fun deleteIssue(issueId: String) = issuesRepository.deleteIssue(issueId)
 
